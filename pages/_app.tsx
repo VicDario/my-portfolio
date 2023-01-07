6c259086a73a5a55e6cc4d56c6e9a72b89d21f10
VicDario <26585356+VicDario@users.noreply.github.com>
2023-01-07 17:09:29 -0300

    [Change] Using next font

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,8 +1,15 @@
 import '@styles/globals.scss';
 import type { AppProps } from 'next/app';
+import { Fira_Code } from '@next/font/google';
+
+const firaCode = Fira_Code({ subsets: ['latin'] });
 
 function MyApp({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />;
+  return (
+    <main className={firaCode.className}>
+      <Component {...pageProps} />
+    </main>
+  );
 }
 
 export default MyApp;
