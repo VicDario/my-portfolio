b8b52eb8c9112c66a9a86686407dab6e13207641
VicDario <26585356+VicDario@users.noreply.github.com>
2023-01-08 00:40:29 -0300

    [Add] Contact button component

---
+++ components/contact-button/contact-button.component.tsx
@@ -0,0 +1,15 @@
+import React from 'react';
+import styles from './contact-button.module.scss';
+
+const ContactButton: React.FC = () => {
+  return (
+    <a
+      className={styles.button}
+      href="&#109;&#97;&#105;&#108;&#116;&#111;&#58;%64%61%72%69%6F%63%6F%6E%74%72%65%72%61%73%66%63%40%67%6D%61%69%6C%2E%63%6F%6D"
+    >
+      Say Hello
+    </a>
+  );
+};
+
+export default ContactButton;
