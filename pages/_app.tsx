cce5c559b23f109cd6b341b103f0720455ec36a3
VicDario <26585356+VicDario@users.noreply.github.com>
2023-02-18 19:15:20 -0300

    [Change] Fontawesome config

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,6 +1,9 @@
 import '@styles/globals.scss';
 import type { AppProps } from 'next/app';
 import { Lato } from '@next/font/google';
+import { config } from '@fortawesome/fontawesome-svg-core';
+import '@fortawesome/fontawesome-svg-core/styles.css';
+config.autoAddCss = false;
 
 const lato = Lato({
   subsets: ['latin'],
