fb80de10bb2c0ff8bb93bd6fd167e3003d391bf4
VicDario <26585356+VicDario@users.noreply.github.com>
2023-02-18 23:31:59 -0300

    [Add] Desktop styles

--- components/skills/skills.components.tsx
+++ components/skills/skills.components.tsx
@@ -7,37 +7,39 @@ import styles from './skills.module.scss';
 const Skills: React.FC = () => {
   return (
     <section className={styles.skills}>
-      <h2>Skills</h2>
-      <div className={styles['skill-item']}>
-        <FontAwesomeIcon icon={faUserCircle} className={styles['skill-icon']} />
-        <h3>Personal</h3>
-        <ul>
-          <li>Organization</li>
-          <li>Proactivity</li>
-          <li>Responsability</li>
-          <li>Teamwork</li>
-        </ul>
-      </div>
-      <div className={styles['skill-item']}>
-        <FontAwesomeIcon icon={faJs} className={styles['skill-icon']} />
-        <h3>Languages</h3>
-        <ul>
-          <li>JavaScript</li>
-          <li>TypeScript</li>
-        </ul>
-      </div>
-      <div className={styles['skill-item']}>
-        <FontAwesomeIcon icon={faLaptopCode} className={styles['skill-icon']} />
-        <h3>Technologies</h3>
-        <ul>
-          <li>Node.js</li>
-          <li>Express</li>
-          <li>GraphQL</li>
-          <li>React</li>
-          <li>Next.js</li>
-          <li>Angular</li>
-        </ul>
-      </div>
+      <h2 className={styles.title}>Skills</h2>
+      <section className={styles['skills-container']}>
+        <article className={styles['skill-item']}>
+          <FontAwesomeIcon icon={faUserCircle} className={styles['skill-icon']} />
+          <h3>Personal</h3>
+          <ul>
+            <li>Organization</li>
+            <li>Proactivity</li>
+            <li>Responsability</li>
+            <li>Teamwork</li>
+          </ul>
+        </article>
+        <article className={styles['skill-item']}>
+          <FontAwesomeIcon icon={faJs} className={styles['skill-icon']} />
+          <h3>Languages</h3>
+          <ul>
+            <li>JavaScript</li>
+            <li>TypeScript</li>
+          </ul>
+        </article>
+        <article className={styles['skill-item']}>
+          <FontAwesomeIcon icon={faLaptopCode} className={styles['skill-icon']} />
+          <h3>Technologies</h3>
+          <ul>
+            <li>Node.js</li>
+            <li>Express</li>
+            <li>GraphQL</li>
+            <li>React</li>
+            <li>Next.js</li>
+            <li>Angular</li>
+          </ul>
+        </article>
+      </section>
     </section>
   );
 };
