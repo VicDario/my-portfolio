bd4c9033c84dc2894c77ae2d502b1e170a427e7f
VicDario <26585356+VicDario@users.noreply.github.com>
2024-12-28 18:10:17 -0300

    build: set astro config

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,5 +1,17 @@
 // @ts-check
 import { defineConfig } from 'astro/config';
 
+import vercel from '@astrojs/vercel';
+
+import icon from 'astro-icon';
+
 // https://astro.build/config
-export default defineConfig({});
+export default defineConfig({
+  adapter: vercel(),
+  integrations: [icon({
+    include: {
+      'fa6-solid': ['circle-user', 'laptop-code'],
+      'fa6-brands': ['js', 'github', 'linkedin']
+    }
+  })]
+});
